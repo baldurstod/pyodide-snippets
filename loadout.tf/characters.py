8ad62fbc07946f388ebf6dd337ea02ba79df6db0
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-11-21 11:38:25 +0100

    Added characters / items snippets

--- loadout.tf/characters.py
+++ loadout.tf/characters.py
@@ -8,41 +8,31 @@ character = await characters.selectCharacter(
     tf2.Scout  # a number can be also be used: 0-9 for humans, 100-109 for bots
 )
 
-# Change the team
+##################################################
+### Change the team
+##################################################
+import characters
+import tf2
+
+character = await characters.selectCharacter(tf2.Scout)
 character.setTeam(tf2.Blu)  # default team is Red
 
-
 ##################################################
-### Add an item
-##################################################
-import characters
-import items
-import tf2
-
-# Init item list
-await items.initItems()
-# await items.initTournamentMedals() # uncomment if you need tournanment medals
-# await items.initWorkshopItems() # uncomment if you need workshop items
-character = await characters.selectCharacter(tf2.Demoman)
-template = items.getItemTemplate("47")
-if template:
-    item = await character.addItem(template)
-    # Change the paint color
-    item.setPaint(tf2.Paints.PinkAsHell)
-
-
-##################################################
-### Add a weapon
+### Set invulnerable
 ##################################################
 import characters
-import items
 import tf2
 
-await items.initItems()
-character = await characters.selectCharacter(tf2.Demoman)
-template = items.getItemTemplate(206)  # gl
-if template:
-    item = await character.addItem(template)
-    item.showFestivizer(True)
-    item.critBoost()
-    item.setStatClock(1234) # None to remove the stat clock
+character = await characters.selectCharacter(tf2.Scout)
+character.setInvulnerable(True)
+
+##################################################
+### Gold / ice ragdoll
+##################################################
+import characters
+import tf2
+
+character = await characters.selectCharacter(tf2.Scout)
+character.setRagdoll(tf2.GoldRagdoll)
+#character.setRagdoll(tf2.IceRagdoll)
+#character.setRagdoll(None)
---
+++ loadout.tf/items.py
@@ -0,0 +1,76 @@
+##################################################
+### Add an item
+##################################################
+import characters
+import items
+import tf2
+
+# Init item list
+await items.initItems()
+# await items.initTournamentMedals() # uncomment if you need tournanment medals
+# await items.initWorkshopItems() # uncomment if you need workshop items
+character = await characters.selectCharacter(tf2.Demoman)
+template = items.getItemTemplate(47)
+if template:
+    item = await character.addItem(template)
+    # Change the paint color
+    item.setPaint(tf2.Paints.PinkAsHell)
+
+##################################################
+### Add a weapon
+##################################################
+import characters
+import items
+import tf2
+
+await items.initItems()
+character = await characters.selectCharacter(tf2.Demoman)
+template = items.getItemTemplate(206)  # gl
+if template:
+    item = await character.addItem(template)
+    item.showFestivizer(True)
+    item.critBoost()
+
+##################################################
+### Add a stat clock
+##################################################
+import characters
+import items
+import tf2
+
+await items.initItems()
+character = await characters.selectCharacter(tf2.Demoman)
+template = items.getItemTemplate(206)  # gl
+if template:
+    item = await character.addItem(template)
+    item.setStatClock(1234)  # item.setStatClock(None) to remove the stat clock
+
+##################################################
+### Add a specialized killstreak
+##################################################
+import characters
+import items
+import tf2
+
+await items.initItems()
+character = await characters.selectCharacter(tf2.Demoman)
+template = items.getItemTemplate(206)  # gl
+if template:
+    item = await character.addItem(template)
+    item.setSheen(tf2.KillstreakColor.VillainousViolet)
+
+##################################################
+### Set a warpaint on a weapon
+##################################################
+import characters
+import items
+import tf2
+
+await items.initItems()
+character = await characters.selectCharacter(tf2.Demoman)
+template = items.getItemTemplate(206)  # gl
+if template:
+    item = await character.addItem(template)
+    # Warpaint id, wear and seed
+    # Wear has a value of 0 (FN) to 4 (BS)
+    item.setWarpaint(200, 0, 0)
